404d0db3cd3c4d33d038c8e71d2e8392eb773447
Fahid Hasan Fuad <fahidhasanfuad20018@gmail.com>
2023-01-07 18:30:04 +0600

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav-items");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navItems.classList.toggle("active");
+});
+
+document.querySelectorAll(".item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navItems.classList.remove("active");
+  })
+);
